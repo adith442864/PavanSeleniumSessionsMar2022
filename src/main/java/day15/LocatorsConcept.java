2b97fd2748783097dc2d65665a2ce099ad164123
“adith442864” <adith442864@gmail.com>
2022-03-22 19:28:12 +0530

    220322_01

--- src/main/java/day15/LocatorsConcept.java
+++ src/main/java/day15/LocatorsConcept.java
@@ -18,7 +18,7 @@ public class LocatorsConcept {
 	
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
-		driver.get("http://automationpractice.com/index.php");
+//		driver.get("http://automationpractice.com/index.php");
 //		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
@@ -37,18 +37,32 @@ public class LocatorsConcept {
 //		driver.findElement(By.partialLinkText("Chiffon Dress")).click();
 		
 		//classname
-		List<WebElement> sliderList = driver.findElements(By.className("homeslider-container"));
-		System.out.println("Total number of images in a slider :"+sliderList.size());
+//		List<WebElement> sliderList = driver.findElements(By.className("homeslider-container"));
+//		System.out.println("Total number of images in a slider :"+sliderList.size());
+//		
+//		for(WebElement e: sliderList) {
+//			String text = e.getText();
+//			System.out.println(text);
+//		}
 		
-		for(WebElement e: sliderList) {
-			String text = e.getText();
-			System.out.println(text);
-		}
 		
+//		List<WebElement> linksList = driver.findElements(By.tagName("a"));
+//		System.out.println("Total no of links in a page :"+linksList.size());
 		
-		List<WebElement> linksList = driver.findElements(By.tagName("a"));
-		System.out.println("Total no of links in a page :"+linksList.size());
+		//facebook app:
+		driver.get("https://facebook.com");
 		
+		//Tag & ID
+		driver.findElement(By.cssSelector("input#email")).sendKeys("test@gmail.com");
+		driver.findElement(By.cssSelector("#email")).sendKeys("test@gmail.com");
+		
+		//tag and class:
+		driver.findElement(By.cssSelector("input.inputtext")).sendKeys("test@gmail.com");
+		driver.findElement(By.cssSelector(".inputtext")).sendKeys("test@gmail.com");
+		
+		//Tag & Class & Attribute
+		driver.findElement(By.cssSelector("input.inputtext[id='email']")).sendKeys("test@gmail.com");
+		driver.findElement(By.cssSelector(".inputtext[id='email']")).sendKeys("test@gmail.com");
 		
 		Thread.sleep(5000);
 		driver.quit();
