9c5f85a59723b5c36c44dbb31fa8a6d109b14d9d
“adith442864” <adith442864@gmail.com>
2022-03-22 19:10:23 +0530

    220322_pavan

---
+++ src/main/java/day14/FirstTestCase.java
@@ -0,0 +1,42 @@
+package day14;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FirstTestCase {
+	
+	static WebDriver driver;
+
+	public static void main(String[] args) throws InterruptedException {
+	
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/index.php/auth/login");
+		
+		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+		driver.findElement(By.id("btnLogin")).click();
+		
+		Thread.sleep(3000);
+		String title = driver.getTitle();
+		
+		if(title.contains("OrangeHRM")) {
+			System.out.println("Login Test is passed.");
+		}else {
+			System.out.println("Login test is failed...");
+		}
+		
+		
+		Thread.sleep(5000);
+		driver.quit();
+		
+		
+		
+		
+
+	}
+
+}
