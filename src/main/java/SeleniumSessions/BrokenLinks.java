1c7421e2a3e9e95fedb7d4cb2590644ba0808edd
“adith442864” <adith442864@gmail.com>
2022-03-26 13:08:59 +0530

    260322_01

--- src/main/java/SeleniumSessions/BrokenLinks.java
+++ src/main/java/SeleniumSessions/BrokenLinks.java
@@ -31,21 +31,34 @@ public class BrokenLinks {
 		
 		for(WebElement e: allLinks) {
 			String url = e.getAttribute("href");
+			
 			if(url==null || url.isEmpty() || url.isBlank()) {
 				System.out.println("URL is empty");
 			}
 			
 			URL link = new URL(url);
+			
 			try {
 				HttpURLConnection httpcon = (HttpURLConnection)link.openConnection();
 				httpcon.connect();
-			}catch(Exception ex) {
+				
+				if(httpcon.getResponseCode()>=400) {
+					System.out.println(httpcon.getResponseCode()+" " +url+ "  "+ "is a broken link");
+					brokenLinks++;
+				}
+				else {
+					System.out.println(httpcon.getResponseCode()+" " +url+ "  " + "is a valid link");
+				}
 				
 			}
+			catch(Exception ex) {
+				ex.printStackTrace();
+			}
+			//System.out.println("Number of broken links: " +brokenLinks);
 		}
 		
 		
-		
+		System.out.println("Number of broken links: " +brokenLinks);
 		
 		Thread.sleep(5000);
 		driver.quit();
