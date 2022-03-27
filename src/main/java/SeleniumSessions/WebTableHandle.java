97421cd9d50b6fbea25241acddfbe38a6fef0b7e
adith <adith@adiths-Mac-mini.local>
2022-03-27 13:40:25 +0530

    270322

---
+++ src/main/java/SeleniumSessions/WebTableHandle.java
@@ -0,0 +1,84 @@
+package SeleniumSessions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/*
+ * How many rows in a table
+ * How many cols in a table
+ * Retrive the specific row/column data
+ * Retrieve all data from the table
+ * Print Release date, 
+ */
+
+public class WebTableHandle {
+
+	static WebDriver driver;
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		
+		driver.get("https://www.w3schools.com/html/html_tables.asp");
+		
+		//How many rows in the table:
+		int rows = driver.findElements(By.xpath("//table[@id='customers']/tbody/tr")).size();
+		System.out.println(rows); //7
+		
+		//How many columns in the table:
+		
+		int cols = driver.findElements(By.xpath("//table[@id='customers']/tbody//tr/th")).size();
+		System.out.println(cols); //3
+		
+		// Retrieve specific row/col data:
+
+		//		//*[@id="customers"]/tbody/tr[2]/td[1]
+		//		//*[@id="customers"]/tbody/tr[2]/td[2]
+		//		//*[@id="customers"]/tbody/tr[2]/td[3]
+		
+		//		//*[@id="customers"]/tbody/tr[3]/td[1]
+		
+//		System.out.println(driver.findElement(By.xpath("//*[@id='customers']/tbody/tr[2]/td[1]")).getText());
+		
+		//Retrieve all rows/cols from the table:
+//		for(int r=2; r<=rows; r++) {
+//			
+//			for(int c=1; c<=cols; c++) {
+//				String value = driver.findElement(By.xpath("//*[@id='customers']/tbody/tr["+r+"]/td["+c+"]")).getText();
+//				System.out.print(value +" --------- "); //
+//				
+//			}
+//			
+//			System.out.println( );
+//		}
+		
+		//Print specific rows data 
+		for(int r=2; r<=rows; r++) {
+			String names = driver.findElement(By.xpath("//*[@id='customers']/tbody/tr["+r+"]/td[1]")).getText();
+			System.out.println(names);
+			if(names.equals("Island Trading")) {
+				String contact = driver.findElement(By.xpath("//*[@id='customers']/tbody/tr["+r+"]/td[2]")).getText();
+				String country = driver.findElement(By.xpath("//*[@id='customers']/tbody/tr["+r+"]/td[3]")).getText();
+				
+				System.out.println(names+"   "+contact +"   "+country);
+				break;
+			}
+			
+			
+		}
+		
+		
+		
+		Thread.sleep(5000);
+		driver.quit();
+		
+		
+		
+
+	}
+
+}
---
+++ src/main/java/SeleniumSessions/WebTableHandle2.java
@@ -0,0 +1,50 @@
+package SeleniumSessions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class WebTableHandle2 {
+
+	static WebDriver driver;
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		
+		driver.get("https://opensource-demo.orangehrmlive.com/index.php/auth/login");
+		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+		driver.findElement(By.id("btnLogin")).click();
+		
+		Thread.sleep(2000);
+		
+		driver.findElement(By.id("menu_admin_viewAdminModule")).click();
+		driver.findElement(By.id("menu_admin_UserManagement")).click();
+		driver.findElement(By.id("menu_admin_viewSystemUsers")).click();
+		
+		// how many users are enabled and disabled. 
+		
+		int rows = driver.findElements(By.xpath("//table[@id='resultTable']/tbody/tr")).size();
+		int count =0;
+		for(int r=1; r<=rows; r++) {
+			String status = driver.findElement(By.xpath("//table[@id='resultTable']/tbody/tr["+r+"]/td[5]")).getText();
+			if(status.equals("Enabled")) {
+				count++;
+			}
+		}
+		
+		System.out.println("No of enabled users: " +count);
+		System.out.println("No of disabled users: " +(rows - count));
+		
+		Thread.sleep(5000);
+		driver.quit();
+		
+		
+
+	}
+
+}
