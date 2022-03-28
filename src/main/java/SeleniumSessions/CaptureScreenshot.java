aff64b74f572cae13821c6417479bf499d3de769
adith442864 <adith442864@gmail.com>
2022-03-28 22:04:48 +0530

    280322_002

---
+++ src/main/java/SeleniumSessions/CaptureScreenshot.java
@@ -0,0 +1,51 @@
+package SeleniumSessions;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CaptureScreenshot {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://demo.nopcommerce.com/");
+		driver.manage().window().maximize();
+		
+		//screenshot of full page:
+		/*
+		TakesScreenshot ts = (TakesScreenshot)driver;
+		File src = ts.getScreenshotAs(OutputType.FILE);
+		File target = new File(System.getProperty("user.dir")+"\\screenshots\\homepage.png");
+		FileUtils.copyFile(src, target);
+		*/
+		
+		//screenshot of particular portion of the page
+		/*
+		WebElement pageSection = driver.findElement(By.xpath("//div[@class='product-grid home-page-product-grid']"));
+		File src = pageSection.getScreenshotAs(OutputType.FILE);
+		File target = new File(System.getProperty("user.dir")+"\\screenshots\\featuredProduct.png");
+		FileUtils.copyFile(src, target);
+		*/
+		
+		//Screenshot of the specific webelement
+		WebElement logo = driver.findElement(By.xpath("//img[@alt='nopCommerce demo store']"));
+		File src = logo.getScreenshotAs(OutputType.FILE);
+		File target = new File(System.getProperty("user.dir")+"\\screenshots\\logo.png");
+		FileUtils.copyFile(src, target);
+		
+		Thread.sleep(5000);
+		driver.quit();
+
+	}
+
+}
---
+++ src/main/java/SeleniumSessions/ScrollingPage.java
@@ -0,0 +1,48 @@
+package SeleniumSessions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ScrollingPage {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.countries-ofthe-world.com/flags-of-the-world.html");
+		driver.manage().window().maximize();
+		
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		/*
+		//1. scroll down page by pixel
+		js.executeScript("window.scrollBy(0,3000)", "");
+		Long value = (Long) js.executeScript("return window.pageYOffset;");
+		System.out.println("Number of pixels moved: " +value); //3000
+		*/
+		
+		//2. Scroll down page till the element is visible
+		/*
+		WebElement flag = driver.findElement(By.xpath(" //img[@alt='Flag of India']"));
+		js.executeScript("arguments[0].scrollIntoView();",flag);
+		Long value = (Long) js.executeScript("return window.pageYOffset;");
+		System.out.println("Number of pixels moved: " +value);
+		*/
+		
+		//3. Scroll down till the end of the page:
+		js.executeScript("window.scrollBy(0,document.body.scrollHeight)");
+		Long value = (Long) js.executeScript("return window.pageYOffset;");
+		System.out.println("Number of pixels moved: " +value);//5916
+		
+		
+		Thread.sleep(5000);
+		driver.quit();
+		
+	}
+
+}
