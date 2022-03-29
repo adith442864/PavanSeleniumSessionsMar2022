2c0d9e28685db4c600dfdcfd03cf35dd1cd5ead4
adith442864 <adith442864@gmail.com>
2022-03-29 21:10:26 +0530

    290322

--- src/main/java/SeleniumSessions/FixedDepositCalculator.java
+++ src/main/java/SeleniumSessions/FixedDepositCalculator.java
@@ -12,64 +12,62 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class FixedDepositCalculator {
 
 	static WebDriver driver;
-	static XLSUtil util;
+//	static XLUtils util;
 
 	public static void main(String[] args) throws InterruptedException, IOException {
 		
 		WebDriverManager.chromedriver().setup();
-		
 		driver = new ChromeDriver();
 		driver.get("https://www.moneycontrol.com/fixed-income/calculator/state-bank-of-india-sbi/fixed-deposit-calculator-SBI-BSB001.html");
 		driver.manage().window().maximize();
 		
 		String path="./src/test/resources/testdata/caldata.xlsx";
-		util = new XLSUtil(path);
+//		util = new XLUtils();
 		
-		int rows = util.getRowCount("Sheet1");
-		
+		int rows = XLUtils.getRowCount(path, "Sheet1");
 		System.out.println(rows);
 
-		
-		Thread.sleep(5000);
 		//capture data from all the columns
 		for(int i=1; i<=rows; i++) {
-			
-			String principle = util.getCellData("Sheet1", i, 0);
-			String rateofinterest = util.getCellData("Sheet1", i, 1);
-			String period1 = util.getCellData("Sheet1", i, 2);
-			String period2 = util.getCellData("Sheet1", i, 3);
-			String frequency = util.getCellData("Sheet1", i, 4);
-			String expected_maturityValue = util.getCellData("Sheet1", i, 5);
-			
-			//application code
-			driver.findElement(By.id("principal")).sendKeys(principle);
-			driver.findElement(By.id("interest")).sendKeys(rateofinterest);
-			driver.findElement(By.id("tenure")).sendKeys(period1);
-			//handle drop-down
-			Select period2DropDown = new Select(driver.findElement(By.id("tenurePeriod")));
-			period2DropDown.selectByVisibleText(period2);
-			
-			Select freqDropDown = new Select(driver.findElement(By.id("frequency")));
-			freqDropDown.selectByVisibleText(frequency);
-			
-			driver.findElement(By.xpath("//*[@id='fdMatVal']/div[2]/a[1]/img")).click();
-			
-			String actual_maturityValue = driver.findElement(By.xpath("//span[@id='resp_matval']//strong")).getText();
-			
-			if(Double.parseDouble(expected_maturityValue) == Double.parseDouble(actual_maturityValue)) {
-				System.out.println("Test Passed");
-				util.setCellData("Sheet1", i, 7, "Passed");
-				util.fillGreenColor("Sheet1", i, 7);
 		
+		String principle = XLUtils.getCellData(path, "Sheet1", i, 0);
+		String rateofinterest = XLUtils.getCellData(path, "Sheet1", i, 1);
+		String period1 = XLUtils.getCellData(path, "Sheet1", i, 2);
+		String period2 = XLUtils.getCellData(path, "Sheet1", i, 3);;
+		String frequency = XLUtils.getCellData(path, "Sheet1", i, 4);
+		String expected_maturityValue = XLUtils.getCellData(path, "Sheet1", i, 5);
+			
+		//application code
+		driver.findElement(By.id("principal")).sendKeys(principle);
+		driver.findElement(By.id("interest")).sendKeys(rateofinterest);
+		driver.findElement(By.id("tenure")).sendKeys(period1);
+		//handle drop-down
+		Select period2DropDown = new Select(driver.findElement(By.id("tenurePeriod")));
+		period2DropDown.selectByVisibleText(period2);
+		
+		Select freqDropDown = new Select(driver.findElement(By.id("frequency")));
+		freqDropDown.selectByVisibleText(frequency);
+		
+		driver.findElement(By.xpath("//*[@id='fdMatVal']/div[2]/a[1]/img")).click();
+		
+		String actual_maturityValue = driver.findElement(By.xpath("//span[@id='resp_matval']//strong")).getText();
+		
+		if(Double.parseDouble(expected_maturityValue) == Double.parseDouble(actual_maturityValue)) {
+			System.out.println("Test Passed");
+			XLUtils.setCellData(path, "Sheet1", i, 7, "Passed");
+			XLUtils.fillGreenColor(path, "Sheet1", i, 7);
+			//util.setCellData("Sheet1", i, 7, "Passed");
+			//util.fillGreenColor("Sheet1", i, 7);
+	
 			
 		}else {
 			System.out.println("Test Failed");
-			util.setCellData("Sheet1", i, 7, "Failed");
-			util.fillRedColor("Sheet1", i, 7);
+			XLUtils.setCellData(path, "Sheet1", i, 7, "Failed");
+			XLUtils.fillRedColor(path, "Sheet1", i, 7);
 		}
 		
 			driver.findElement(By.xpath("//img[@class='PL5']")).click();
-			Thread.sleep(5000);
+			Thread.sleep(3000);
 
 		}
 		
